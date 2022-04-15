9d97ccc2951871729b8c4517a1c28cc34db7642f
davidyim03 <66441490+davidyim03@users.noreply.github.com>
2022-04-15 18:56:58 -0500

    lab3

---
+++ lab03/answers.sql
@@ -0,0 +1,95 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name 
+FROM users;
+
+
+
+-- Exercise 3
+SELECT id, first_name, last_name 
+FROM users 
+ORDER BY last_name ASC;
+
+
+
+-- Exercise 4
+SELECT id, user_id, image_url
+FROM posts
+WHERE user_id = 26;
+
+
+-- Exercise 5
+SELECT id, user_id, image_url
+FROM posts
+WHERE user_id = 26 or user_id = 12;
+
+
+
+-- Exercise 6
+SELECT COUNT(*)
+FROM posts;
+
+
+
+-- Exercise 7
+SELECT user_id, COUNT(*) 
+FROM comments
+GROUP BY user_id;
+
+
+
+-- Exercise 8
+
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name
+FROM posts
+JOIN users ON users.id = posts.user_id
+WHERE user_id = 26 or user_id = 12;
+
+
+-- Exercise 9
+
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts
+JOIN following ON posts.user_id = following.following_id
+WHERE following.user_id = 26;
+
+
+-- Exercise 10
+
+
+
+
+-- Exercise 11
+
+INSERT INTO bookmarks (user_id, post_id, timestamp)
+VALUES(26, 219, CURRENT_TIMESTAMP);
+INSERT INTO bookmarks (user_id, post_id, timestamp)
+VALUES(26, 220, CURRENT_TIMESTAMP);
+INSERT INTO bookmarks (user_id, post_id, timestamp)
+VALUES(26, 221, CURRENT_TIMESTAMP);
+
+
+-- Exercise 12
+DELETE FROM bookmarks
+WHERE post_id = 219 and user_id = 26;
+
+DELETE FROM bookmarks
+WHERE post_id = 220 and user_id = 26;
+
+DELETE FROM bookmarks
+WHERE post_id = 221 and user_id = 26;
+
+
+
+-- Exercise 13
+
+UPDATE users
+SET email = 'knick2022@gmail.com'
+WHERE id = 26;
+
+
+-- Exercise 14
